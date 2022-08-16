56c6d69931525bf19650bf1880693e33c7c445d2
Fernanda <fernandacastropalhap12@gmail.com>
2022-08-16 15:19:08 -0300

    trocando a ordem para crescente

--- exercicio2/src/main/kotlin/exercicio2.1.kt
+++ exercicio2/src/main/kotlin/exercicio2.1.kt
@@ -5,17 +5,17 @@ fun main() {
     val numero2 = readLine()!!.toDouble()
     print("digite outro número: ")
     val numero3 = readLine()!!.toDouble()
-    if(numero1 > numero2 && numero2 > numero3){
+    if(numero1 < numero2 && numero2 < numero3){
         println("$numero1, $numero2, $numero3")}
-    else if(numero1 > numero3 && numero3 > numero2){
+    else if(numero1 < numero3 && numero3 < numero2){
         println("$numero1, $numero3, $numero2")}
-    else if(numero2 > numero1 && numero1 > numero3){
+    else if(numero2 < numero1 && numero1 < numero3){
         println("$numero2, $numero1, $numero3")}
-    else if(numero2 > numero3 && numero3 > numero1){
+    else if(numero2 < numero3 && numero3 < numero1){
         println("$numero2, $numero3, $numero1")}
-    else if(numero3 > numero1 && numero1 > numero2){
+    else if(numero3 < numero1 && numero1 < numero2){
         println("$numero3, $numero1, $numero2")}
-    else if(numero3 > numero2 && numero2 > numero1){
+    else if(numero3 < numero2 && numero2 < numero1){
         println("$numero3, $numero2, $numero1")}
     else{println("não há como fazer uma ordem crescente")}
 }
